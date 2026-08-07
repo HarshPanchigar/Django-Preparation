cba8c139cdc4e4b1bb300f79c987aec1cab1782e
Harsh Panchigar <harshpanchigar@gmail.com>
2026-08-07 23:34:02 +0530

    initial commit

--- myProject12/student/urls.py
+++ myProject12/student/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.student_list, name='student_list'),
     path('add/' , views.student_create , name='student_create'),
     path('details/<int:pk>' , views.student_detail , name='student_detail'),
+    path('edit/<int:pk>' , views.student_edit , name='student_edit'),
+    path('delete/<int:pk>' , views.student_delete , name='student_delete'),
 ]
 
