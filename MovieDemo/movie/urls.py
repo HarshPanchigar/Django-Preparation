a28c10994f606dc50fc891b117160a8cda4d51dd
Harsh Panchigar <harshpanchigar@gmail.com>
2026-08-14 22:13:39 +0530

    inital commit

--- MovieDemo/movie/urls.py
+++ MovieDemo/movie/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.home, name="home"),
+    path("", views.home, name="home"),
     path("movie/<str:imdb_id>/", views.movie_detail, name="movie_detail"),
 ]
--- MovieDemo/movie/views.py
+++ MovieDemo/movie/views.py
@@ -1,59 +1,33 @@
-# import requests
-# from django.shortcuts import render
-
-# API_KEY = "6a4dd6af"
-# BASE_URL = "http://www.omdbapi.com/"
-
-
-# def home(request):
-#     movie_name = request.GET.get("movie", "")
-
-#     movies = []
-
-#     if movie_name:
-#         params = {
-#             "s": movie_name,
-#             "apikey": API_KEY
-#         }
-
-#         response = requests.get(BASE_URL, params=params)
-
-#         if response.status_code == 200:
-#             data = response.json()
-
-#             if data["Response"] == "True":
-#                 movies = data["Search"]
-
-#     return render(request, "index.html", {
-#         "movies": movies,
-#         "movie_name": movie_name
-#     })
-
-
-# def movie_detail(request, imdb_id):
-
-#     params = {
-#         "i": imdb_id,
-#         "apikey": API_KEY
-#     }
-
-#     response = requests.get(BASE_URL, params=params)
-
-#     movie = None
-
-#     if response.status_code == 200:
-#         data = response.json()
-
-#         if data["Response"] == "True":
-#             movie = data
-
-#     return render(request, "details.html", {"movie": movie})
-
 import requests
 from django.shortcuts import render
 
 API_KEY = "6a4dd6af"
 BASE_URL = "http://www.omdbapi.com/"
+
+
+def home(request):
+    movie_name = request.GET.get("movie", "")
+
+    movies = []
+
+    if movie_name:
+        params = {
+            "s": movie_name,
+            "apikey": API_KEY
+        }
+
+        response = requests.get(BASE_URL, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if data["Response"] == "True":
+                movies = data["Search"]
+
+    return render(request, "index.html", {
+        "movies": movies,
+        "movie_name": movie_name
+    })
 
 
 def movie_detail(request, imdb_id):
@@ -65,13 +39,12 @@ def movie_detail(request, imdb_id):
 
     response = requests.get(BASE_URL, params=params)
 
-    movie = {}
+    movie = None
 
     if response.status_code == 200:
-        movie = response.json()
+        data = response.json()
 
-        # Print everything in the terminal
-        from pprint import pprint
-        pprint(movie)
+        if data["Response"] == "True":
+            movie = data
 
     return render(request, "details.html", {"movie": movie})
