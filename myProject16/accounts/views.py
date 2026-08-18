1a7b481704c2a442b0eff0d374a3afc21cf06452
Harsh Panchigar <harshpanchigar@gmail.com>
2026-08-18 17:39:47 +0530

    media

--- myProject16/accounts/views.py
+++ myProject16/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .forms import ProfileForm
+from .models import Profile
+from django.contrib import messages
 
-# Create your views here.
+def upload_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile picture uploaded successfully')
+            return redirect('view_profile')
+        else:
+            messages.error(request,'Error uploading profile picture')
+    else:
+        form = ProfileForm()
+        return render(request,'upload_profile.html', {'form':form})
+
+def view_profile(request):
+    profile = Profile.objects.all()
+    return render(request,'view_profile.html',{'profiles': profile} )
