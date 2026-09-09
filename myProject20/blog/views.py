21f99b9120a430525ddb8420727012b225f5be43
Harsh Panchigar <harshpanchigar@gmail.com>
2026-09-09 23:56:30 +0530

    email

--- myProject20/blog/views.py
+++ myProject20/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 
-# Create your views here.
+# def send_test_email(request):
+#     subject = 'Welcome to my blog'
+#     message = "Thank you for subscribing MY BLOG"
+#     from_email = "harshpanchigar@gmail.com"
+#     recipient_list = ["harshpanchigar40@gmail.com"]
+
+#     send_mail(subject,message,from_email,recipient_list)
+#     return HttpResponse("test  email sent successfully!")
+
+def send_test_email(request):
+    subject = 'Welcome to my blog'
+    message = render_to_string('email/welcome_email.html',{
+        'username' : 'Harsh',
+        'course' : 'Django Course'
+    })
+    email = EmailMessage(
+        subject,
+        message,
+        "harshpanchigar@gmail.com",
+        ["harshpanchigar40@gmail.com"]
+    )
+    email.content_subtype = 'html'
+    email.send()
+    return HttpResponse("test  email sent successfully!")
--- myProject20/myProject20/settings.py
+++ myProject20/myProject20/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'myProject20.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR , 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Mail settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
